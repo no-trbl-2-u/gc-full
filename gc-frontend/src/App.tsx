8cb171ccc3f1c5e8aabd7f908c16f469bff89ffa
unknown <no.trbl.2.u@gmail.com>
2019-02-11 13:20:00 -0500

    Cleared out CRA stuff

--- gc-frontend/src/App.tsx
+++ gc-frontend/src/App.tsx
@@ -1,28 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 
-// TODO: CLEAR THIS ALL OUT
+// TODO: Migrate Old Website to here
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>We made it from the bottom, now we here! Something About how the whole team's here.</h1>
       </div>
     );
   }
